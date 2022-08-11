448f669d9321059e0ac52f2ac46bf4e3045d00fd
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-08-11 05:32:43 -0300

    ok

---
+++ code.js
@@ -0,0 +1,4 @@
+let nombre = document.getElementById("nombreP");
+let precioPorUnidad = document.getElementById("precio");
+let cantidadDeProducto = document.getElementById("cantidad");
+let costeFinal = 0;
