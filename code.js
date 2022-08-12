4b6f34c2802273735c3a74cee1113642f8bfb868
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-08-12 04:16:44 -0300

    ok

--- code.js
+++ code.js
@@ -1,4 +1,28 @@
-let nombre = document.getElementById("nombreP");
-let precioPorUnidad = document.getElementById("precio");
-let cantidadDeProducto = document.getElementById("cantidad");
 let costeFinal = 0;
+
+function agregarProducto() {
+  let nombre = document.getElementById("nombreP").value;
+  let precioPorUnidad = document.getElementById("precio").value;
+  let cantidadDeProducto = document.getElementById("cantidad").value;
+  let listaNueva = document.getElementById("lista");
+  let total = cantidadDeProducto * precioPorUnidad;
+  const objLista = document.createElement("li");
+  listaNueva.appendChild(objLista);
+  objLista.innerHTML =
+    nombre +
+    " " +
+    precioPorUnidad +
+    " -C/u- " +
+    " x " +
+    cantidadDeProducto +
+    " = $" +
+    total;
+  costeFinal += total;
+}
+
+function precioFinal() {
+  let totalida = document.getElementById("listCompras");
+  let resultado = document.createElement("p");
+  resultado.textContent = parseInt(costeFinal);
+  totalida.appendChild(resultado);
+}
