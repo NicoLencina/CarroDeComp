075da7c6ac146d01c272a1ee2bf3ce3ca84e6af8
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-08-14 16:38:58 -0300

    ok

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+console.clear();
 //voy a guardar todos los resultados de las multiplicaciones para dar el total
 let costeFinal = 0;
 
@@ -7,37 +8,41 @@ function agregarProducto() {
   let precioPorUnidad = document.getElementById("precio").value;
   let cantidadDeProducto = document.getElementById("cantidad").value;
   //guardo dentro de el div lista
-  let listaNueva = document.getElementById("lista");
+  let listaNueva = document.querySelector(".listCompras");
   //el resultado de cantidad por producto que voy a guardar en costeFinal
   let total = cantidadDeProducto * precioPorUnidad;
   //creo 4 lista con cada objeto para editar en css
+  let contengo = document.createElement("ul");
   let objetoNombre = document.createElement("li");
   let objetoProducto = document.createElement("li");
   let objetoPrecio = document.createElement("li");
   let precio = document.createElement("li");
   //armo los textos por separado
   let datoNombre = document.createTextNode("Producto: " + nombre);
-  let datoProducto = document.createTextNode(
-    "Precio por unidad: $" + precioPorUnidad
-  );
+  let datoProducto = document.createTextNode("Por unidad: $" + precioPorUnidad);
   let datoPrecio = document.createTextNode("Cantidad: " + cantidadDeProducto);
-  let datoFinal = document.createTextNode(": $" + total);
+  let datoFinal = document.createTextNode("= $" + total);
   //creo
-  listaNueva.appendChild(objetoNombre);
+  listaNueva.appendChild(contengo);
+  contengo.appendChild(objetoNombre);
   objetoNombre.appendChild(datoNombre);
-  listaNueva.appendChild(objetoProducto);
+  contengo.appendChild(objetoProducto);
   objetoProducto.appendChild(datoProducto);
-  listaNueva.appendChild(objetoPrecio);
+  contengo.appendChild(objetoPrecio);
   objetoPrecio.appendChild(datoPrecio);
-  listaNueva.appendChild(precio);
+  contengo.appendChild(precio);
   precio.appendChild(datoFinal);
 
+  costeFinal = costeFinal + total;
   console.log(costeFinal);
 }
 
 function precioFinal() {
-  let totalida = document.getElementById("pFinal");
-  let dato = parseInt(precioFinal);
-  totalida.appendChild(dato);
-  console.log(dato);
+  let final = document.getElementById("laLista");
+  let totalida = document.createElement("p");
+  let textoFinal = document.createTextNode("Total: $" + costeFinal);
+  final.appendChild(totalida);
+  totalida.classList.add("editoFinal");
+  totalida.appendChild(textoFinal);
+  console.log(totalida);
 }
